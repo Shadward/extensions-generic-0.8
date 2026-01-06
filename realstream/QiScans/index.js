3ce82a585c534f5711afd32b2d99d342f173f55c
Shadward <123044231+Shadward@users.noreply.github.com>
2026-01-06 15:39:18 +0200

    Update QiScans version to 0.0.2

--- realstream/QiScans/index.js
+++ realstream/QiScans/index.js
@@ -2275,7 +2275,7 @@ const types_1 = require("@paperback/types");
 const realstream_1 = require("../realstream");
 const DOMAIN = 'https://qiscans.org';
 exports.QiScansInfo = {
-    version: (0, realstream_1.getExportVersion)('0.0.1'),
+    version: (0, realstream_1.getExportVersion)('0.0.2'),
     name: 'QiScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
       author: 'Real',
