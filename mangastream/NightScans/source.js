f2cc9a1c7e665bd657d37c6650da9bd1f0b51c20
Shadward <123044231+Shadward@users.noreply.github.com>
2026-01-06 04:30:56 +0200

    Update DOMAIN and name for NightScans to QiScans

--- mangastream/NightScans/source.js
+++ mangastream/NightScans/source.js
@@ -2273,10 +2273,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NightScans = exports.NightScansInfo = void 0;
 const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
-const DOMAIN = 'https://nightscans.net';
+const DOMAIN = 'https://qiscans.org';
 exports.NightScansInfo = {
     version: (0, MangaStream_1.getExportVersion)('0.0.1'),
-    name: 'NightScans',
+    name: 'QiScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
     authorWebsite: 'http://github.com/TheNetsky',
